695f2524580d7e419fbce948af815511ba65c1e4
ighfar <31362144+ighfar@users.noreply.github.com>
2018-03-30 17:42:57 +0700

    Add files via upload
    Event handle by entering keyboard

---
+++ src/events/e.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package events;
+
+/**
+ *
+ * @author Microsoft
+ */
+class e {
+    static boolean VK_ENTER;
+
+    static boolean getKeyCode() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
